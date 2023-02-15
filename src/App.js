5f3385592192001577784d4dd9b05a64faef4683
Sanjay <sanjaym1998@gmail.com>
2023-02-15 10:05:42 +0530

    Countdown timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [countdown, setCountdown] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+      let countdownInterval = null;
+      if (countdown > 0) {
+         countdownInterval = setInterval(() => {
+            setCountdown(countdown - 1);
+       }, 1000);
+    }
+       return () => clearInterval(countdownInterval);
+  }, [countdown]);
+
+        const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+       const inputValueAsNumber = Math.floor(Number(inputValue));
+        setCountdown(inputValueAsNumber);
+      setInputValue("");
+    }
+  };
+
+  const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+       <h1>Counter Timer</h1>
+      <input
+      
+        id="timeCount"
+        type="text"
+        placeholder='Enter the Counter Time'
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
+          <div id="currenttime"><h1>{countdown}</h1></div>
     </div>
   );
 }
